de1e57dc22ee2b90c42337745dafae307edf5e5e
gracek87 <gracek87@hawaii.edu>
2026-01-22 12:31:36 -1000

    Create ex3.py

---
+++ Lab2/ex3.py
@@ -0,0 +1,10 @@
+# Ask the user to enter a floating point number, square the number, and then pring out the original number and the squared result.
+# Name: Grace Kulhanek
+# Date: 01/22/2026
+
+inputValue = input("Please enter a floating point number: ")
+floatValue = float(inputValue)
+squaredValue = floatValue ** 2
+
+print("The original number is:", floatValue)
+print(f"The squared value is: {squaredValue}")
